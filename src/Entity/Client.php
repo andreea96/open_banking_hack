a376ca4c4243bb0744840d53509405a27189ec59
Dorin <dorinngrigoras@yahoo.com>
2019-09-14 20:41:17 +0300

    added client and cont tables

---
+++ src/Entity/Client.php
@@ -0,0 +1,13 @@
+namespace App\Entity;
+
+class Client
+{
+   private $idClient;
+   private $nume;
+   private $prenume;
+   private $cnp;
+   private $serie;
+   private $numarSerie;
+   private $tel;
+   private $email;
+}
---
+++ src/Entity/Cont.php
@@ -0,0 +1,14 @@
+namespace App\Entity;
+
+class Cont
+{
+   private $idCont;
+   private $idClient;
+   private $iban;
+   private $user;
+   private $password;
+   private $suma;
+   private $valuta;
+   private $userApp;
+   private $passwordApp;
+}
